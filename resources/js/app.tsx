924ae781a701be9e7a4e5b421d76589f492ba16b
Ogier <ogier9@gmail.com>
2022-10-18 17:42:46 +0200

    udpdate version

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 const el = document.getElementById('app')
+if( !el ) {throw null}
 const root = createRoot(el)
 root.render(
   <InertiaApp
     // Pass props from the server down to the client app
-    initialPage={JSON.parse(el.dataset.page)}
+    initialPage={JSON.parse(el.dataset.page as string)}
     // Dynamically load the required page component
     resolveComponent={(name) => import(`./pages/${name}`).then((module) => module.default)} initialComponent={''}
   
